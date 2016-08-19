8e068cd24f9e5b9ba02c3325bc1aa113a747da68
aditi2016 <aditilahoria@gmail.com>
2016-08-19 16:53:38 +0530

    dont

--- blueteam.py
+++ blueteam.py
@@ -1,11 +1,13 @@
+#!/usr/bin/python
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
+usageInMin = {'startAt':int(time.time())}
 
 driver = webdriver.Chrome(executable_path="/home/collap/Downloads/chromedriver.exe")
 
 driver.get("http://goo.gl/545wov")
-time.sleep(20)
+time.sleep(30)
 mobile = driver.find_element_by_name("mobile")
 mobile.send_keys(8750937767)
 password = driver.find_element_by_name("password")
@@ -14,31 +16,74 @@ driver.find_element_by_name("submit").click()
 #driver.find_element_by_link_text("MAID")
 #driver.find_element_by_name("MAID").click()
 #driver.close(1000)
-time.sleep(50)
+time.sleep(90)
 
-#driver.find_element_by_xpath('//a[img/@src="http://blueteam.in/static/images/maid.jpeg"]').click()
-driver.find_element_by_xpath("//img[contains(@src,'maid.jpeg')]").click()
-time.sleep(20)
-driver.get("http://blueteam.in/web-app/#/tab/service/maid/type/On-Demand")
+driver.find_element_by_css_selector("body > ion-nav-view > ion-side-menus > ion-side-menu-content > ion-nav-bar > div:nth-child(4) > ion-header-bar > div.buttons.buttons-left.header-item > span > button").click()
+time.sleep(5)
 
 
-#first get all the <a> elements
-#List<WebElement> linkList=driver.findElements(By.tagName("a"));
+driver.find_element_by_css_selector("body > ion-nav-view > ion-side-menus > ion-side-menu > ion-content > div.scroll > ion-list > div > ion-item:nth-child(3) > a").click()
+time.sleep(10)
+driver.back()
+time.sleep(10)
+driver.find_element_by_css_selector("body > ion-nav-view > ion-side-menus > ion-side-menu-content > ion-nav-bar > div:nth-child(4) > ion-header-bar > div.buttons.buttons-left.header-item > span > button").click()
 
-#//now traverse over the list and check
-#for(int i=0 ; i<linkList.size() ; i++)
-#{
- #   if(linkList.get(i).getAttribute("href").contains("long"))
-  #  {
-  #      linkList.get(i).click();
- 
- #       break;
-  #  }
-#}
+
+driver.find_element_by_css_selector("body > ion-nav-view > ion-side-menus > ion-side-menu > ion-content > div.scroll > ion-list > div > ion-item:nth-child(6) > a").click()
+time.sleep(10)
+driver.back()
+time.sleep(10)
+driver.find_element_by_css_selector("body > ion-nav-view > ion-side-menus > ion-side-menu-content > ion-nav-bar > div:nth-child(4) > ion-header-bar > div.buttons.buttons-left.header-item > span > button").click()
+
+
+driver.find_element_by_css_selector("body > ion-nav-view > ion-side-menus > ion-side-menu > ion-content > div.scroll > ion-list > div > ion-item:nth-child(7) > a").click()
+time.sleep(10)
+driver.back()
+time.sleep(10)
+driver.find_element_by_css_selector("body > ion-nav-view > ion-side-menus > ion-side-menu-content > ion-nav-bar > div:nth-child(4) > ion-header-bar > div.buttons.buttons-left.header-item > span > button").click()
+
+driver.find_element_by_css_selector("body > ion-nav-view > ion-side-menus > ion-side-menu > ion-content > div.scroll > ion-list > div > ion-item:nth-child(9) > a").click()
+time.sleep(10)
+driver.back()
+time.sleep(10)
+driver.find_element_by_css_selector("body > ion-nav-view > ion-side-menus > ion-side-menu-content > ion-nav-bar > div:nth-child(4) > ion-header-bar > div.buttons.buttons-left.header-item > span > button").click()
+
+
+driver.find_element_by_css_selector("body > ion-nav-view > ion-side-menus > ion-side-menu > ion-content > div.scroll > ion-list > div > ion-item:nth-child(12) > a").click()
+time.sleep(10)
+driver.back()
+time.sleep(10)
+driver.find_element_by_css_selector("body > ion-nav-view > ion-side-menus > ion-side-menu-content > ion-nav-bar > div:nth-child(4) > ion-header-bar > div.buttons.buttons-left.header-item > span > button").click()
+
+
+driver.find_element_by_css_selector("body > ion-nav-view > ion-side-menus > ion-side-menu > ion-content > div.scroll > ion-list > div > ion-item:nth-child(5) > a").click()
+time.sleep(10)
+driver.back()
+time.sleep(10)
+driver.find_element_by_css_selector("body > ion-nav-view > ion-side-menus > ion-side-menu-content > ion-nav-bar > div:nth-child(4) > ion-header-bar > div.buttons.buttons-left.header-item > span > button").click()
+
+
+driver.find_element_by_css_selector("body > ion-nav-view > ion-side-menus > ion-side-menu > ion-content > div.scroll > ion-list > div > ion-item:nth-child(14)").click()
+time.sleep(10)
+driver.back()
+time.sleep(10)
+driver.find_element_by_css_selector("body > ion-nav-view > ion-side-menus > ion-side-menu-content > ion-nav-bar > div:nth-child(4) > ion-header-bar > div.buttons.buttons-left.header-item > span > button").click()
+
+driver.close()
+    
+
+
+
+
+
+
 
 
 
     
+
+
+
 
 
 
