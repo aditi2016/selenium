8ada46653784e5ca1bbf5254306d0345fd747fcb
aditi2016 <aditilahoria@gmail.com>
2016-08-22 13:10:42 +0530

    done

---
+++ registration.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+usageInMin = {'startAt':int(time.time())}
+#path of chrome driver
+driver = webdriver.Chrome(executable_path="/home/collap/Downloads/chromedriver.exe")
+# go to that url
+driver.get("http://goo.gl/545wov")
+#wait for 30second while page loading
+time.sleep(30)
+#find element by name
+mobile = driver.find_element_by_name("mobile")
+#then send key or can say password where moblile in css
+mobile.send_keys(7239889257)
+#send password at place of password
+password = driver.find_element_by_name("password")
+password.send_keys("redhat@11111p")
+time.sleep(10)
+conf_password = driver.find_element_by_name("conf_password")
+conf_password.send_keys("redhat@11111p")
+name = driver.find_element_by_name("name")
+name.send_keys("redhat@11111p")
+email = driver.find_element_by_name("email")
+email.send_keys("lahoria.singh.aditi@gmail.com")
+driver.find_element_by_name("submit").click()
