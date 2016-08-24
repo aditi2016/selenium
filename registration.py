4f6719260b1681c50b3988c5c58db8a782c1abc9
anil-ninja <kumar.anil8892@gamil.com>
2016-08-24 10:49:41 +0530

    format properly

--- registration.py
+++ registration.py
@@ -5,12 +5,12 @@ import smtplib
 from smtplib import SMTPException
 usageInMin = {'startAt':int(time.time())}
 #path of chrome driver
-driver = webdriver.Chrome(executable_path="/home/collap/Downloads/chromedriver.exe")
+driver = webdriver.Chrome(executable_path="/home/ishu/Downloads/chromedriver")
 # go to that url
 try:
 	driver.get("http://goo.gl/545wov")
 	#wait for 30second while page loading
-	#time.sleep(30)
+	time.sleep(30)
 	#find element by name
 	mobile = driver.find_element_by_name("mobile")
 	#then send key or can say password where moblile in css
@@ -29,29 +29,30 @@ try:
 	driver.close()
 
 
-except:sender = 'aditilahoria@gmail.com'
-receivers = ['aditilahoria@gmail.com']
+except:
+	sender = 'aditilahoria@gmail.com'
+	receivers = ['aditilahoria@gmail.com']
 
-message = """From: From Person <den.callanan@gmail.com>
-To: To Person <callanden@gmail.com>
-Subject: SMTP e-mail test
+	message = """From: From Person <den.callanan@gmail.com>
+	To: To Person <callanden@gmail.com>
+	Subject: SMTP e-mail test
 
-code error code error.
-"""
+	code error code error.
+	"""
 
-try:
-    print("trying host and port...")
+	try:
+	    print("trying host and port...")
 
-    smtpObj = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-    smtpObj.login("aditilahoria@gmail.com", "killbill12345")
+	    smtpObj = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+	    smtpObj.login("aditilahoria@gmail.com", "killbill12345")
 
-    print("sending mail...")
+	    print("sending mail...")
 
-    smtpObj.sendmail(sender, receivers, message)
+	    smtpObj.sendmail(sender, receivers, message)
 
-    print("Succesfully sent email")
+	    print("Succesfully sent email")
 
-except smtplib.SMTPException:
-    print("Error: unable to send email")
-    import traceback
-    traceback.print_exc()
+	except smtplib.SMTPException:
+	    print("Error: unable to send email")
+	    import traceback
+	    traceback.print_exc()
